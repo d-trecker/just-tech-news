1c41fa88e7fb8f8ad2c50fe68f6cbbd2d06bb1a3
Dylan Trecker <dylantrecker@WarMachineAir.local>
2021-06-09 15:47:01 -0700

    Created all post routes

--- models/Post.js
+++ models/Post.js
@@ -1,41 +1,42 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-//create our post model 
+// create our Post model
 class Post extends Model {}
 
-// creates fields /columns for post model 
+// creates fields /columns for post model
 Post.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false, 
-            primaryKey: true,
-            autoIncrement: true
-        },
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        post_url: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                isUrl: true
-            }
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'user',
-                key: 'id'
-            }
-        }
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'post'
-    }
-)
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post_url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isUrl: true,
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "post",
+  }
+);
+module.exports = Post;
